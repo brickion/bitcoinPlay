60c89c744870afe88409c509b19269c3b3396bb2
Roy Hui <24906507+brickion@users.noreply.github.com>
2018-04-19 10:10:47 +1000

    more btc methods

--- index.js
+++ index.js
@@ -1,33 +1,26 @@
-var bitcoin = require('bitcoinjs-lib')
-var got = require('got');
+const bitcoin = require('bitcoinjs-lib')
+const axios = require('axios')
+const production = false
+const network = production ? bitcoin.networks.bitcoin : bitcoin.networks.testnet
+const blockchainInfoURL = production ? 'https://blockchain.info' : 'https://testnet.blockchain.info'
 
-//console.log(CreateTestAccount())
-GetTransaction('29804e2ada8afb98b11d4ac288b5ec41cf6a789aa2251342dfec0cc16d06b51b')
+//console.log(CreateAccount())
+//GetTransaction('29804e2ada8afb98b11d4ac288b5ec41cf6a789aa2251342dfec0cc16d06b51b')
+//GetBalance('n4BFE8vKYmxgRHSxcgJ75MB4SQSbRSn8De')
 
 function Send(from, key, to, amount) {
   var key = bitcoin.ECKey.fromWIF(key)
-  var tx = new bitcoin.TransactionBuilder();
-  tx.addInput(from, amount);
-  tx.addOutput(to, amount);
-  tx.sign(0, key);
-  console.log(tx.build().toHex());
-  // POST https://blockchain.info/pushtx
-  // should post to our own node
+  var tx = new bitcoin.TransactionBuilder()
+  tx.addInput(from, amount)
+  tx.addOutput(to, amount)
+  tx.sign(0, key)
+  console.log(tx.build().toHex())
+  const res = await axios.post(blockchainInfoURL + '/pushtx', {tx:tx.build().toHex()})
+  console.log(res.data)
 }
 
 function CreateAccount() {
-  var keyPair = bitcoin.ECPair.makeRandom()//{ network: testnet})
-  var address = keyPair.getAddress()
-  var key = keyPair.toWIF()
-  return {
-    address: address,
-    key: key
-  }
-}
-
-function CreateTestAccount() {
-  var testnet = bitcoin.networks.testnet
-  var keyPair = bitcoin.ECPair.makeRandom({ network: testnet})
+  var keyPair = bitcoin.ECPair.makeRandom({network: network})
   var address = keyPair.getAddress()
   var key = keyPair.toWIF()
   return {
@@ -37,11 +30,11 @@ function CreateTestAccount() {
 }
 
 async function GetTransaction(transactionHash) {
-  const res = await got('https://testnet.blockchain.info/rawtx/' + transactionHash)
-  console.log(res)
+  const res = await axios.get(blockchainInfoURL + '/rawtx/' + transactionHash)
+  console.log(res.data)
 }
 
 async function GetBalance(address) {
-  const res = await got('https://testnet.blockchain.info/balance?active=' + address)
-  console.log(res)
+  const res = await axios.get(blockchainInfoURL + '/balance?active=' + address)
+  console.log(res.data)
 }
